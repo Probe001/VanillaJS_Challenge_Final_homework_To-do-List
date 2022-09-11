079cb48abfcc79eb8a3558d2fbc48b470e7e0551
Probe001 <hojong123456@naver.com>
2022-09-12 05:14:16 +0900

    rebuild

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 function randomBg() {
   const bodyEl = document.querySelector("body");
-  const url = `/images/${Math.floor(Math.random() * 3)}.jpg`;
+  const url = `./images/${Math.floor(Math.random() * 3)}.jpg`;
   bodyEl.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(${url})`;
 }
 randomBg();
